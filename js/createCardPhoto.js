ffc73d38e9869b4b2e7c03caf4dea45ae3d80350
Pavlo Cherevach <pf4pad.fr@gmail.com>
2022-06-19 16:34:33 +0300

    ScrollAndEmptyIndex

--- js/createCardPhoto.js
+++ js/createCardPhoto.js
@@ -1,14 +1,24 @@
 import { createElem } from "./createElem.js";
 
 const loadImage = (url, description) => {
-  const img = new Image();
-  img.width = '200';
-  img.src = url;
-  img.alt = description;
-  return img;
+
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.width = '200';
+    img.src = url;
+    img.alt = description;
+    img.addEventListener('load', () => {
+      resolve(img)
+    });
+    img.addEventListener('error', () => {
+      reject(new Error(err))
+    })
+  })
+
+
 }
 
-export const createCardPhoto = (data) => {
+export const createCardPhoto = async (data) => {
   const card = createElem('li', {
     className: 'card'
   });
@@ -20,7 +30,7 @@ export const createCardPhoto = (data) => {
   });;
 
 
-  const photo = loadImage(data.urls.small, data.alt_decription)
+  const photo = await loadImage(data.urls.small, data.alt_decription);
 
   const author = createElem('a', {
     id: data.id,
--- js/renderGallery.js
+++ js/renderGallery.js
@@ -1,11 +1,14 @@
 import { createCardPhoto } from "./createCardPhoto.js";
 import { createElem } from "./createElem.js";
 
+import { scrollLoad } from "./ScrollLoad.js";
+
 export const renderGallery = (wrapper, photos) => {
   const gallery = createElem('ul', {
     className: 'grid',
   });
   wrapper.append(gallery);
+  const endElem = createElem('div')
 
   const grid = new Masonry(gallery, {
     gutter: 10,
@@ -15,6 +18,13 @@ export const renderGallery = (wrapper, photos) => {
   })
 
   const cards = photos.map(createCardPhoto);
-  gallery.append(...cards);
-  grid.appended(cards)
+
+  Promise.all(cards)
+    .then(cards => {
+      gallery.append(...cards);
+      grid.appended(cards);
+      wrapper.append(endElem);
+      scrollLoad(gallery, grid, endElem)
+    })
+
 }
